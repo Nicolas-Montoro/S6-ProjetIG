f1e265736399abed4f714120d87852c79d045978
Vincent-Fabioux <vincent.fabioux@u-psud.fr>
2016-05-08 21:18:04 +0200

    Ajouts de plein de trucs

--- projet.c
+++ projet.c
@@ -1,33 +1,32 @@
-/*****************************************************************************
-File: TD3-avatar-skel.c
+/***********************************************************************
+* Projet d'informatique graphique
+* Par Vincent Fabioux et Nicolas Montoro
+* Polytech Paris Sud 2016 - S6 INFO
+* 
+* Commande de compilation:
+* gcc -o projet projet.c -lGL -lGLU -lglut -lm
+* VÃ©rifier que OpenGL est installÃ©:
+* glxinfo | grep OpenGL
+* Installer les librairies "freeglut-dev" et "libtiff-dev".
+***********************************************************************/
 
-Informatique Graphique IG1
-IFIPS
-Christian Jacquemin, Université Paris 11
-
-Copyright (C) 2007 University Paris 11 
-This file is provided without support, instruction, or implied
-warranty of any kind.  University Paris 11 makes no guarantee of its
-fitness for a particular purpose and is not liable under any
-circumstances for any damages or loss whatsoever arising from the use
-or inability to use this file or items derived from it.
-******************************************************************************/
 #include <GL/gl.h>           
 #include <GL/glu.h>         
 #include <GL/glut.h>
 #include <stdio.h>      
 #include <stdlib.h>     
 #include <math.h>
-#include <tiffio.h>     /* Sam Leffler's libtiff library. */
+#include <tiffio.h>
+
 int writetiff(char *filename, char *description,
 	      int x, int y, int width, int height, int compression);
 
-#define    windowWidth 800
-#define    windowHeight 800
+#define windowWidth		800
+#define windowHeight	800
 
 #define RED   0
-#define GREEN 0
-#define BLUE  0
+#define GREEN 0.5
+#define BLUE  1
 #define ALPHA 1
 
 #define true  1
@@ -37,26 +36,30 @@ int writetiff(char *filename, char *description,
 
 #define PI 3.1415926535898
 
-#define position_Ini                   -30.0
+#define MAP_SIZE	100
 
-float t = 0.f;
-float delta = 0.1f;
-float k = 0.001f;
-float K = 0.002f;
+/* Variables de gestion du temps */
+int Step = 0;
+int latence = 4;
+float delta = 1.0;
 int IdleRunning = 1;
-int TraceEcran = 0;
-int RangFichierStockage = 0;
-float position = position_Ini;
+
+/* Variables de la camÃ©ra */
+float cameraPos[3] = {0, -15, -20};
+int cameraSpeed = 1;
+int cameraLimits[6] = {-1*(MAP_SIZE/2-20), (MAP_SIZE/2-20),
+	-35, -15,
+	-1*(MAP_SIZE/2-20), (MAP_SIZE/2-20)};
 
 
-// Enumération des formes possibles
+// EnumÃ©ration des formes possibles
 enum ShapeList
 {
-	Cube,
+	Cube = 1,
 	Sphere,
-	Cylindre
+	Cylindre,
+	Disque
 };
-
 
 // Structure d'un type d'objet (id, forme, taille[3])
 typedef struct
@@ -64,41 +67,74 @@ typedef struct
 	int id;
 	int shape;
 	float size[3];
+	float color[3];
 } ObjectType;
 
-
-// Structure d'un objet (type, position[3], matrice)
+// Structure d'un objet (type, position[3], angle[3], matrice)
 typedef struct
 {
 	int type;
 	float pos[3];
+	float angle[4];
 	int matrix;
 } Object;
 
 
-// Enumération des noms d'objets
-#define OBJECTS_TYPE_COUNT	2
+
+/* EnumÃ©ration des noms d'objets */
 enum ObjectName
 {
+	Sol = 1,
+	Herbe,
+	Soleil,
+	Mur,
+	ChaiseAssise,
 	ChaisePied,
-	ChaiseAssise
+	ChaiseBarre,
+	ChaiseDossier
 };
 
-
-// Forme et taille de chaque type d'objet
-ObjectType objectsType[OBJECTS_TYPE_COUNT] = {
-	{ChaisePied, Cube, {1, 5, 1}},
-	{ChaiseAssise, Cube, {7, 1, 7}}
+/* Forme et taille de chaque type d'objet:
+ * {Nom, Forme, {tailleX, tailleY, tailleZ}, {R, G, B}}
+ */
+ObjectType objectsType[] = {
+	{Sol, Cube, {MAP_SIZE, 3, MAP_SIZE}, {0.7, 0.7, 0.7}},
+	{Herbe, Disque, {0, 1000, 0}, {0, 0.7, 0}},
+	{Soleil, Sphere, {7000, 1, 1}, {1, 1, 0}},
+	{Mur, Cube, {MAP_SIZE+1.5, 30, 3}, {0.3, 0.3, 0.3}},
+	{ChaiseAssise, Cube, {7, 1, 7}, {0.6, 0.3, 0}},
+	{ChaisePied, Cube, {1, 5, 1}, {0.6, 0.3, 0}},
+	{ChaiseBarre, Cube, {1, 4, 1}, {0.6, 0.3, 0}},
+	{ChaiseDossier, Cube, {7, 4, 1}, {0.6, 0.3, 0}},
+	
+	{0, 0, {0, 0, 0}}
 };
 
-#define OBJECTS_COUNT	5
-Object objects[OBJECTS_COUNT] = {
+/* Type, position et angle de chaque objet:
+ * {Type, {posX, posY, posZ}, {angle, x, y, z}, +-matrix}
+ */
+Object objectsL[] = {
+	{Sol, {0, -1.5, 0}, {0, 0, 0, 0}, 1},
+	{Herbe, {0, 0, 0}, {90, 1, 0, 0}, 0},
+	{Soleil, {500, 0, -100000}, {0, 0, 0, 0}, -1},
+	{Mur, {0, 15, MAP_SIZE/2}, {0, 0, 0, 0}, 1},
+		{Mur, {0, 0, -1*MAP_SIZE}, {0, 0, 0, 0}, 0},
+		{Mur, {MAP_SIZE/2, 0, MAP_SIZE/2}, {90, 0, 1, 0}, 0},
+	{Mur, {0, 0, -1*MAP_SIZE}, {0, 0, 0, 0}, -1},
 	// Chaise
-	{ChaisePied, {-3, 0, -3}, 1},
-	{ChaisePied, {6, 0, 0}, 0},
-	{ChaisePied, {0, 0, 6}, 0},
-	{ChaisePied, {-6, 0, 0}, -1},
-	{ChaiseAssise, {0, 3, 0}, 0}
+	{ChaiseAssise, {0, 6.5, -20}, {0, 0, 1, 0}, 1},
+		{ChaisePied, {-3, -3, -3}, {0, 0, 0, 0}, 1},
+			{ChaisePied, {6, 0, 0}, {0, 0, 0, 0}, 0},
+			{ChaisePied, {0, 0, 6}, {0, 0, 0, 0}, 0},
+		{ChaisePied, {-6, 0, 0}, {0, 0, 0, 0}, -1},
+		{ChaiseBarre, {-3, 2.5, -3}, {0, 0, 0, 0}, 1},
+		{ChaiseBarre, {6, 0, 0}, {0, 0, 0, 0}, -1},
+	{ChaiseDossier, {0, 6.5, -3}, {0, 0, 0, 0}, -1},
+	//Bonhomme
+	
+	
+	
+	{0, {0, 0, 0}, {0, 0, 0}, 0}
 };
 	
 
@@ -110,7 +146,7 @@ static GLfloat mat_specular[] = { 1.0 , 1.0 , 1.0 , 1.0 };
 static GLfloat mat_ambientanddiffuse[] = { 0.4, 0.4 , 0.0 , 1.0 };
 static GLfloat mat_shininess[] = { 20.0};
 
-static GLfloat light_position0[] = { 15.0 , 15.0 , 15.0 , 0.0 };
+static GLfloat light_position0[] = { 5.0 , 15.0 , 15.0 , 0.0 };
 static GLfloat light_position1[] = { 15.0 , 15.0 , -15.0 , 0.0 };
 
 static GLfloat ambient_light0[] = { 0.0 , 0.0 , 0.0 , 0.0 };
@@ -121,16 +157,14 @@ static GLfloat ambient_light1[] = { 0.50 , 0.50 , 0.50 , 1.0 };
 static GLfloat diffuse_light1[] = { 0.5 , 1.0 , 1.0 , 1.0 };
 static GLfloat specular_light1[] = { 0.5 , 1.0 , 1.0 , 1.0 };
 
+
+/* Variables de gestion de la souris */
 int angle_x = 0, angle_y = 0;
 int mouse_pos_x = 0, mouse_pos_y = 0;
 int mouse_down_is_left = 0;
 
-int Step = 0;
-int latence = 4;
 
-void init_scene();
 void render_scene();
-void init_angles();
 GLvoid initGL();
 GLvoid window_display();
 GLvoid window_reshape(GLsizei width, GLsizei height); 
@@ -139,56 +173,68 @@ GLvoid window_timer();
 GLvoid window_mouseFunc(int button, int state, int x, int y);
 GLvoid window_motionFunc(int x, int y);
 void Faire_Composantes();
+void checkCameraLimits();
 
 void init_cube(ObjectType object);
 void init_cylinder(ObjectType object, GLUquadricObj* qobj);
 void init_sphere(ObjectType object);
+void init_disk(ObjectType object, GLUquadricObj* qobj);
 
 
 int main(int argc, char **argv) 
 {  
-  // initialisation  des paramètres de GLUT en fonction
-  // des arguments sur la ligne de commande
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	// Initialisation  des paramÃ¨tres de GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
-  // définition et création de la fenêtre graphique
-  glutInitWindowSize(windowWidth,windowHeight);
-  glutInitWindowPosition(0, 0);
-  glutCreateWindow("Projet d'informatique systeme");
+	// DÃ©finition et crÃ©ation de la fenÃªtre graphique
+	glutInitWindowSize(windowWidth,windowHeight);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Projet d'informatique systeme");
 
-  // initialisation de OpenGL et de la scène
-  initGL();  
-  init_scene();
+	// Initialisation de OpenGL et de la scÃ¨ne
+	initGL(); 
+	Faire_Composantes(); 
 
-  // choix des procédures de callback pour 
-  // le tracé graphique
-  glutDisplayFunc(&window_display);
-  // le redimensionnement de la fenêtre
-  glutReshapeFunc(&window_reshape);
-  // la gestion des événements clavier
-  glutKeyboardFunc(&window_key);
-  // fonction appelée régulièrement entre deux gestions d´événements
-  glutTimerFunc(latence,&window_timer,Step);
-  // la gestion des clicks souris
-  glutMouseFunc(&window_mouseFunc);
-  // la gestion des déplacement souris
-  glutMotionFunc(&window_motionFunc);
+	// Choix du callback pour le tracÃ© graphique...
+	glutDisplayFunc(&window_display);
+	// .. pour le redimensionnement de la fenÃªtre
+	glutReshapeFunc(&window_reshape);
+	// ... pour la gestion des Ã©vÃ©nements clavier
+	glutKeyboardFunc(&window_key);
+	// .. pour le timer
+	glutTimerFunc(latence,&window_timer,Step);
+	// ... pour les clicks souris
+	glutMouseFunc(&window_mouseFunc);
+	// ... pour les dÃ©placement souris
+	glutMotionFunc(&window_motionFunc);
 
-  // la boucle prinicipale de gestion des événements utilisateur
-  glutMainLoop();  
+	// Boucle principale de gestion des Ã©venements
+	glutMainLoop();  
 
-  return 1;
+	return 1;
 }
 
-// initialisation du fond de la fenêtre graphique : noir opaque
 
+/* Gestion du timer */
+GLvoid window_timer() 
+{
+	if(IdleRunning)
+	{
+		objectsL[7].angle[0] += delta;
+	}
+	glutTimerFunc(latence,&window_timer,++Step);
+	glutPostRedisplay();
+}
+
+
+// Initialisation d'OpenGL
 GLvoid initGL() 
 {
-  // initialisation de l´éclairement
+  // Initialisation de lÂ´Ã©clairement
 	glEnable(GL_COLOR_MATERIAL);
 
-  // définition de deux source lumineuses
+  // DÃ©finition de deux source lumineuses
   glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_light0);
   glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse_light0);
   glLightfv(GL_LIGHT0, GL_SPECULAR, specular_light0);
@@ -198,32 +244,26 @@ GLvoid initGL()
   glLightfv(GL_LIGHT0, GL_POSITION, light_position0);
   glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
 
-  // activation de l´éclairement
+  // Activation de lÂ´Ã©clairage
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
   glEnable(GL_LIGHT1);
 
-  // propriétés matérielles des objets
+  // PropriÃ©tÃ©s matÃ©rielles des objets
   glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, mat_ambientanddiffuse);
   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
   glShadeModel( GL_SMOOTH );
   glEnable(GL_COLOR_MATERIAL);
 
-  // initialisation du fond
+  // Initialisation du fond
   glClearColor(RED, GREEN, BLUE, ALPHA);        
-  // z-buffer
+  // Activation du z-buffer
   glEnable(GL_DEPTH_TEST);
 }
 
-void init_scene()
-{
-  // initialise des display lists des composantes cylindriques du corps
-  Faire_Composantes();
-}
 
-// fonction de call-back pour l´affichage dans la fenêtre
-
+// Affichage de la fenÃªtre
 GLvoid window_display()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -231,66 +271,96 @@ GLvoid window_display()
 
   render_scene();
 
-  // Tracé la scène grapnique qui vient juste d'être définie
+  // TracÃ© la scÃ¨ne grapnique qui vient juste d'Ãªtre dÃ©fini
   glFlush();
 }
 
-// fonction de call-back pour le redimensionnement de la fenêtre
 
+/* Gestion du redimensionnement de la fenÃªtre */
 GLvoid window_reshape(GLsizei width, GLsizei height)
 {  
 	glViewport(0, 0, width, height);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-20, 20, -20, 20, -1000, 1000);
-	// glFrustum(-20, 20, -20, 20, 10, 1000);
+	// glOrtho(-20, 20, -20, 20, -1000, 1000);
+	glFrustum(-10, 10, -10, 10, 10, 1000000);
 	// glScalef(10, 10, 10);
 
-	// toutes les transformations suivantes s´appliquent au modèle de vue 
+	// Toutes les transformations suivantes sÂ´appliquent au modÃ¨le de vue 
 	glMatrixMode(GL_MODELVIEW);
 }
 
-// fonction de call-back pour la gestion des événements clavier
 
+/* Gestion des Ã©venements clavier */
 GLvoid window_key(unsigned char key, int x, int y) 
 {  
-	switch (key) {    
-		case KEY_ESC:  
-			exit(1);                    
-			break; 
-		case 's':  
-			if (IdleRunning) {
-				glutTimerFunc(latence,NULL,Step);
+	switch (key) {
+		case KEY_ESC:
+			exit(1);
+			break;
+		case 'p': // Pause
+			if (IdleRunning)
 				IdleRunning = 0;
-			} 
-			else {
-				glutTimerFunc(latence,&window_timer,Step);
+			else
 				IdleRunning = 1;
-			}
-			break; 
-		case '+':  
+			break;
+		case '+':
 			delta *= 1.01;
 			break; 
-		case '-':  
+		case '-':
 			delta /= 1.01;
-			break; 
-		default:
-			printf ("La touche %d n´est pas active.\n", key);
+			break;
+			
+		case 'z':
+			cameraPos[0] -= sin(angle_y*PI/180) * cameraSpeed;
+			cameraPos[2] += cos(angle_y*PI/180) * cameraSpeed;
+			checkCameraLimits();
+			break;
+		case 's':
+			cameraPos[0] += sin(angle_y*PI/180) * cameraSpeed;
+			cameraPos[2] -= cos(angle_y*PI/180) * cameraSpeed;
+			checkCameraLimits();
+			break;
+		case 'q':
+			cameraPos[0] += cos(angle_y*PI/180) * cameraSpeed;
+			cameraPos[2] += sin(angle_y*PI/180) * cameraSpeed;
+			checkCameraLimits();
+			break;
+		case 'd':
+			cameraPos[0] -= cos(angle_y*PI/180) * cameraSpeed;
+			cameraPos[2] -= sin(angle_y*PI/180) * cameraSpeed;
+			checkCameraLimits();
+			break;
+		case 'a':
+			cameraPos[1] -= cameraSpeed;
+			if(cameraPos[1] < cameraLimits[2])
+				cameraPos[1] = cameraLimits[2];
+			break;
+		case 'e':
+			cameraPos[1] += cameraSpeed;
+			if(cameraPos[1] > cameraLimits[3])
+				cameraPos[1] = cameraLimits[3];
 			break;
 	}
 }
 
-// fonction de call-back appelée régulièrement
 
-GLvoid window_timer() 
+void checkCameraLimits()
 {
-	glutTimerFunc(latence,&window_timer,++Step);
-	glutPostRedisplay();
+	if(cameraPos[0] < cameraLimits[0])
+		cameraPos[0] = cameraLimits[0];
+	if(cameraPos[0] > cameraLimits[1])
+		cameraPos[0] = cameraLimits[1];
+	if(cameraPos[2] < cameraLimits[4])
+		cameraPos[2] = cameraLimits[4];
+	if(cameraPos[2] > cameraLimits[5])
+		cameraPos[2] = cameraLimits[5];
 }
 
-// fonction de call-back de gestion des clicks souris
 
+
+/* Gestion des clicks de la souris */
 GLvoid window_mouseFunc(int button, int state, int x, int y)
 {
 	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON)
@@ -305,8 +375,8 @@ GLvoid window_mouseFunc(int button, int state, int x, int y)
 	}
 }
 
-// fonction de call-back de gestion des déplacement de la souris
 
+/* Gestion des dÃ©placements de la souris */
 GLvoid window_motionFunc(int x, int y)
 {
 	if( !mouse_down_is_left )
@@ -322,23 +392,26 @@ GLvoid window_motionFunc(int x, int y)
 }
 
 
-// Initialisation des objets
+/* Initialisation des objets */
 void Faire_Composantes() {
 	
-	// Allocation d´une description de quadrique
+	// Allocation dÂ´une description de quadrique
 	GLUquadricObj* GLAPIENTRY qobj;
 	qobj = gluNewQuadric();
 	gluQuadricDrawStyle(qobj, GLU_FILL); 
 	gluQuadricNormals(qobj, GLU_SMOOTH);
 
-	int listStart = (int) glGenLists(OBJECTS_TYPE_COUNT), i;
-	for(i = 0; i < OBJECTS_TYPE_COUNT; i++)
+	// GÃ©nÃ©ration des emplacements de display list
+	int i;
+	for(i = 0; objectsType[i].id; i++);
+	int listStart = (int) glGenLists(i);
+	for(i = 0; objectsType[i].id; i++)
 		objectsType[i].id += listStart;
-	for(i = 0; i < OBJECTS_COUNT; i++)
-		objects[i].type += listStart;
+	for(i = 0; objectsL[i].type; i++)
+		objectsL[i].type += listStart;
 
 	// Compilation des display lists
-	for(i = 0; i < OBJECTS_TYPE_COUNT; i++)
+	for(i = 0; objectsType[i].id; i++)
 	{
 		switch(objectsType[i].shape)
 		{
@@ -351,15 +424,20 @@ void Faire_Composantes() {
 			case Sphere:
 				init_sphere(objectsType[i]);
 				break;
+			case Disque:
+				init_disk(objectsType[i], qobj);
+				break;
 		}
 	}
 }
 
 
+/* Display list de cube */
 void init_cube(ObjectType object)
 {
 	glNewList(object.id, GL_COMPILE);
 		glPushMatrix();
+			glColor3f(object.color[0], object.color[1], object.color[2]);
 			glScalef(object.size[0], object.size[1], object.size[2]);
 			glutSolidCube(1.0);
 		glPopMatrix();
@@ -367,44 +445,59 @@ void init_cube(ObjectType object)
 }
 
 
+/* Display list de cylindre */
 void init_cylinder(ObjectType object, GLUquadricObj* qobj)
 {
 	glNewList(object.id, GL_COMPILE);
+		glColor3f(object.color[0], object.color[1], object.color[2]);
 		gluCylinder(qobj, object.size[0], object.size[1], object.size[2], 20, 20);
 	glEndList();
 }
 
 
+/* Display list de sphere */
 void init_sphere(ObjectType object)
 {
 	glNewList(object.id, GL_COMPILE);
+		glColor3f(object.color[0], object.color[1], object.color[2]);
 		glScalef(1, object.size[1], object.size[2]);
 		glutSolidSphere(object.size[0], 20, 20);
 	glEndList();
 }
 
 
+void init_disk(ObjectType object, GLUquadricObj* qobj)
+{
+	glNewList(object.id, GL_COMPILE);
+		glColor3f(object.color[0], object.color[1], object.color[2]);
+		gluDisk(qobj, object.size[0], object.size[1], 20, 20);
+	glEndList();
+}
+
+/* Rendu de la scÃ¨ne */
 void render_scene()
 {
 	// Rotation de la souris
 	glRotatef(angle_x, 1, 0, 0);
 	glRotatef(angle_y, 0, 1, 0);
-
-	glColor3f(1.0, 1.0, 1.0);
+	
+	// CoordonnÃ©e de la camÃ©ra
+	glTranslatef(cameraPos[0], cameraPos[1], cameraPos[2]);
 	
 	int i, j;
 	
 	// Affichage de tout les objets
-	for(i = 0; i < OBJECTS_COUNT; i++)
+	for(i = 0; objectsL[i].type; i++)
 	{
-		j = objects[i].matrix;
+		j = objectsL[i].matrix;
 		while(j > 0)
 		{
 			glPushMatrix();
 			j--;
 		}
-		glTranslatef(objects[i].pos[0], objects[i].pos[1], objects[i].pos[2]);
-		glCallList(objects[i].type);
+		glTranslatef(objectsL[i].pos[0], objectsL[i].pos[1], objectsL[i].pos[2]);
+		glRotatef(objectsL[i].angle[0], objectsL[i].angle[1], objectsL[i].angle[2], objectsL[i].angle[3]);
+		glCallList(objectsL[i].type);
 		while(j < 0)
 		{
 			glPopMatrix();
@@ -412,6 +505,6 @@ void render_scene()
 		}
 	}
 
-	// Permutation des buffers lorsque le tracé est achevé
+	// Permutation des buffers lorsque le tracÃ© est achevÃ©
 	glutSwapBuffers();
 }
